e2a3de9583b21da003d9bad5f83460648850429a
elieltonk2 <146467618+elieltonk2@users.noreply.github.com>
2026-03-09 00:07:31 -0400

    Create App_simple.tsx

---
+++ src/App_simple.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center p-4">
+      <h1 className="text-white text-4xl">PROZIN HOTSPOT DEBUG</h1>
+    </div>
+  );
+}
